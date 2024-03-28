3ebdabe4ec3fb89bbe1c0b2c2b913d8f4799bd63
NO <NO>
2024-03-28 15:17:53 -0400

    Window Creation Used SDL to create a simple window.

--- Particle-Simulation/src/mian.cpp
+++ Particle-Simulation/src/mian.cpp
@@ -2,13 +2,35 @@
 #include "SDL.h"
 
 int main(int argc, char* argv[]) {
+	
+	const int SCREEN_WIDTH = 800;
+	const int SCREEN_HEIGHT = 600;
+
 	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
 		std::cout << "SDL initialization failed" << std::endl;
 		return 1;
 	}
 
-	std::cout << "SDL initialization succedded" << std::endl;
+	SDL_Window* window = SDL_CreateWindow("Particle Simulation", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
 
+	if (window == nullptr) {
+		SDL_Quit();
+		return 2;
+	}
+
+	bool exit_program = false;
+	SDL_Event event;
+
+	while (!exit_program) {
+
+		while (SDL_PollEvent(&event)) {
+			if (event.type == SDL_QUIT) {
+				exit_program = true;
+			}
+		}
+	}
+
+	SDL_DestroyWindow(window);
 	SDL_Quit();
 
 	return 0;
