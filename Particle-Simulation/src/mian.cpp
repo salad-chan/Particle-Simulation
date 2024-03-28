b3c992624c59b5ebc87870bea771bf80b33d3874
NO <NO>
2024-03-28 14:37:04 -0400

    Post-Build Behavior Fixed post-build behavior issue in Visual Studio project can be built now

--- Particle-Simulation/src/mian.cpp
+++ Particle-Simulation/src/mian.cpp
@@ -1,9 +1,15 @@
+#include <iostream>
 #include "SDL.h"
 
 int main(int argc, char* argv[]) {
 	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+		std::cout << "SDL initialization failed" << std::endl;
 		return 1;
 	}
+
+	std::cout << "SDL initialization succedded" << std::endl;
+
+	SDL_Quit();
 
 	return 0;
 }
