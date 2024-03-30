9472b3f8bf9385ba84e6876f77d53d731778d195
NO <NO>
2024-03-30 00:16:02 -0400

    Pixel Level Access Program renders a white screen

--- Particle-Simulation/src/mian.cpp
+++ Particle-Simulation/src/mian.cpp
@@ -6,21 +6,54 @@ int main(int argc, char* argv[]) {
 	const int SCREEN_WIDTH = 800;
 	const int SCREEN_HEIGHT = 600;
 
+	// Initialization of SDL
 	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
 		std::cout << "SDL initialization failed" << std::endl;
 		return 1;
 	}
 
+	// Window Creation
 	SDL_Window* window = SDL_CreateWindow("Particle Simulation", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
 
+	// Window Check
 	if (window == nullptr) {
 		SDL_Quit();
 		return 2;
 	}
 
+	// Creation of SLD renderer, last argument prevents screen tearing
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC);
+
+	if (renderer == nullptr) {
+		std::cout << "Renderer could not be creater." << std::endl;
+		SDL_DestroyWindow(window);
+		SDL_Quit();
+		return 3;
+	}
+
+	SDL_Texture* texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ABGR8888, SDL_TEXTUREACCESS_STATIC, SCREEN_WIDTH, SCREEN_HEIGHT);
+
+	if (texture == nullptr) {
+		std::cout << "Texture could not be creater." << std::endl;
+		SDL_DestroyRenderer(renderer);
+		SDL_DestroyWindow(window);
+		SDL_Quit();
+		return 4;
+	}
+
+	Uint32* buffer = new Uint32[SCREEN_WIDTH*SCREEN_HEIGHT];
+
+	memset(buffer, 0xff, SCREEN_WIDTH*SCREEN_HEIGHT*sizeof(Uint32));
+
 	bool exit_program = false;
 	SDL_Event event;
 
+	SDL_UpdateTexture(texture, nullptr, buffer, SCREEN_WIDTH*sizeof(Uint32));
+	SDL_RenderClear(renderer);
+	SDL_RenderCopy(renderer, texture, nullptr, nullptr);
+	SDL_RenderPresent(renderer);
+
+	// Main loop
 	while (!exit_program) {
 
 		while (SDL_PollEvent(&event)) {
@@ -30,6 +63,11 @@ int main(int argc, char* argv[]) {
 		}
 	}
 
+	delete[] buffer;
+
+	// Cleanup after SDL
+	SDL_DestroyTexture(texture);
+	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(window);
 	SDL_Quit();
 
